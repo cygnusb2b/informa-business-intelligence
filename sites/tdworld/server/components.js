7be8881ca600fd44ac4d9b96c5a7ccc2d3ff2712
Jacob Bare <jacob@limit0.io>
2019-11-16 21:29:12 -0600

    Use common components in content and page templates

--- sites/tdworld/server/components.js
+++ sites/tdworld/server/components.js
@@ -1,9 +1,9 @@
-const ContentLoadMoreFlow = require('@endeavor-business-media/lazarus-shared/components/flows/content-load-more');
-const ContentPageLoadMoreFlow = require('@endeavor-business-media/lazarus-shared/components/flows/content-page-load-more');
-const ContentListFlow = require('@endeavor-business-media/lazarus-shared/components/flows/content-list');
+const LazarusSharedContentLoadMoreFlow = require('@endeavor-business-media/lazarus-shared/components/flows/content-load-more');
+const LazarusSharedContentPageLoadMoreFlow = require('@endeavor-business-media/lazarus-shared/components/flows/content-page-load-more');
+const LazarusSharedContentListFlow = require('@endeavor-business-media/lazarus-shared/components/flows/content-list');
 
 module.exports = {
-  ContentLoadMoreFlow,
-  ContentPageLoadMoreFlow,
-  ContentListFlow,
+  LazarusSharedContentLoadMoreFlow,
+  LazarusSharedContentPageLoadMoreFlow,
+  LazarusSharedContentListFlow,
 };
