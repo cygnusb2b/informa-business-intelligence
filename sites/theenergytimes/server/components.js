343fbefa0cafb1cdab7e0a0cc4a17c7d2cd4ddaf
Brandon Krigbaum <brandonbk@gmail.com>
2019-11-17 10:50:41 -0600

    Delete theenergytimes site

--- sites/theenergytimes/server/components.js
+++
@@ -1,9 +0,0 @@
-const LazarusSharedContentLoadMoreFlow = require('@endeavor-business-media/lazarus-shared/components/flows/content-load-more');
-const LazarusSharedContentPageLoadMoreFlow = require('@endeavor-business-media/lazarus-shared/components/flows/content-page-load-more');
-const LazarusSharedContentListFlow = require('@endeavor-business-media/lazarus-shared/components/flows/content-list');
-
-module.exports = {
-  LazarusSharedContentLoadMoreFlow,
-  LazarusSharedContentPageLoadMoreFlow,
-  LazarusSharedContentListFlow,
-};
