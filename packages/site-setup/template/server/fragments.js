a873ebc7d58a0e085d38ca7ebfe787ac3411b84c
Jacob Bare <jacob@limit0.io>
2019-11-17 10:25:17 -0600

    Create new site template

---
+++ packages/site-setup/template/server/fragments.js
@@ -0,0 +1,7 @@
+const contentList = require('@endeavor-business-media/lazarus-shared/graphql/fragments/content-list');
+const contentPage = require('@endeavor-business-media/lazarus-shared/graphql/fragments/content-page');
+
+module.exports = {
+  contentList,
+  contentPage,
+};
