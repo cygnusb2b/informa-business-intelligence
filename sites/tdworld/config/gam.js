7903e5bec6ccbfaa0aae5c20d6585b40ad3b6501
Jacob Bare <jacob@limit0.io>
2019-11-16 20:19:16 -0600

    Set tdworld confog

--- sites/tdworld/config/gam.js
+++ sites/tdworld/config/gam.js
@@ -1,6 +1,6 @@
 const GAMConfiguration = require('@base-cms/marko-web-gam/config');
 
-const config = new GAMConfiguration('3834', { basePath: 'iw.home' });
+const config = new GAMConfiguration('3834', { basePath: 'tdworld.home' });
 
 config
   .setTemplate('lb-main', {
@@ -31,60 +31,6 @@ config
     { name: 'mr-section-page', templateName: 'mr', path: 'homepage' },
     { name: 'mr-content-page', templateName: 'mr', path: 'homepage' },
     { name: 'sponsored-logo-section-page', templateName: 'sponsored-logo', path: 'homepage' },
-  ])
-  .setAliasAdUnits('talent', [
-    { name: 'lb-section-page-main', templateName: 'lb-main', path: 'categories/talent' },
-    { name: 'lb-content-page-main', templateName: 'lb-main', path: 'article/talent' },
-    { name: 'lb-section-page-list', templateName: 'lb-list', path: 'categories/talent' },
-    { name: 'lb-content-page-list', templateName: 'lb-list', path: 'article/talent' },
-    { name: 'mr-section-page', templateName: 'mr', path: 'categories/talent' },
-    { name: 'mr-content-page', templateName: 'mr', path: 'article/talent' },
-    { name: 'sponsored-logo-section-page', templateName: 'sponsored-logo', path: 'categories/talent' },
-  ])
-  .setAliasAdUnits('technology-and-iiot', [
-    { name: 'lb-section-page-main', templateName: 'lb-main', path: 'categories/technology_and_iiot' },
-    { name: 'lb-content-page-main', templateName: 'lb-main', path: 'article/technology_and_iiot' },
-    { name: 'lb-section-page-list', templateName: 'lb-list', path: 'categories/technology_and_iiot' },
-    { name: 'lb-content-page-list', templateName: 'lb-list', path: 'article/technology_and_iiot' },
-    { name: 'mr-section-page', templateName: 'mr', path: 'categories/technology_and_iiot' },
-    { name: 'mr-content-page', templateName: 'mr', path: 'article/technology_and_iiot' },
-    { name: 'sponsored-logo-section-page', templateName: 'sponsored-logo', path: 'categories/technology_and_iiot' },
-  ])
-  .setAliasAdUnits('operations', [
-    { name: 'lb-section-page-main', templateName: 'lb-main', path: 'categories/operations' },
-    { name: 'lb-content-page-main', templateName: 'lb-main', path: 'article/operations' },
-    { name: 'lb-section-page-list', templateName: 'lb-list', path: 'categories/operations' },
-    { name: 'lb-content-page-list', templateName: 'lb-list', path: 'article/operations' },
-    { name: 'mr-section-page', templateName: 'mr', path: 'categories/operations' },
-    { name: 'mr-content-page', templateName: 'mr', path: 'article/operations' },
-    { name: 'sponsored-logo-section-page', templateName: 'sponsored-logo', path: 'categories/operations' },
-  ])
-  .setAliasAdUnits('leadership', [
-    { name: 'lb-section-page-main', templateName: 'lb-main', path: 'categories/leadership' },
-    { name: 'lb-content-page-main', templateName: 'lb-main', path: 'article/leadership' },
-    { name: 'lb-section-page-list', templateName: 'lb-list', path: 'categories/leadership' },
-    { name: 'lb-content-page-list', templateName: 'lb-list', path: 'article/leadership' },
-    { name: 'mr-section-page', templateName: 'mr', path: 'categories/leadership' },
-    { name: 'mr-content-page', templateName: 'mr', path: 'article/leadership' },
-    { name: 'sponsored-logo-section-page', templateName: 'sponsored-logo', path: 'categories/leadership' },
-  ])
-  .setAliasAdUnits('supply-chain', [
-    { name: 'lb-section-page-main', templateName: 'lb-main', path: 'categories/supply_chain' },
-    { name: 'lb-content-page-main', templateName: 'lb-main', path: 'article/supply_chain' },
-    { name: 'lb-section-page-list', templateName: 'lb-list', path: 'categories/supply_chain' },
-    { name: 'lb-content-page-list', templateName: 'lb-list', path: 'article/supply_chain' },
-    { name: 'mr-section-page', templateName: 'mr', path: 'categories/supply_chain' },
-    { name: 'mr-content-page', templateName: 'mr', path: 'article/supply_chain' },
-    { name: 'sponsored-logo-section-page', templateName: 'sponsored-logo', path: 'categories/supply_chain' },
-  ])
-  .setAliasAdUnits('the-economy', [
-    { name: 'lb-section-page-main', templateName: 'lb-main', path: 'categories/the_economy' },
-    { name: 'lb-content-page-main', templateName: 'lb-main', path: 'article/the_economy' },
-    { name: 'lb-section-page-list', templateName: 'lb-list', path: 'categories/the_economy' },
-    { name: 'lb-content-page-list', templateName: 'lb-list', path: 'article/the_economy' },
-    { name: 'mr-section-page', templateName: 'mr', path: 'categories/the_economy' },
-    { name: 'mr-content-page', templateName: 'mr', path: 'article/the_economy' },
-    { name: 'sponsored-logo-section-page', templateName: 'sponsored-logo', path: 'categories/the_economy' },
   ]);
 
 module.exports = config;
--- sites/tdworld/config/site.js
+++ sites/tdworld/config/site.js
@@ -5,31 +5,28 @@ module.exports = {
   navigation,
   logos: {
     navbar: {
-      src: 'https://base.imgix.net/files/base/ebm/iw/static/iw_logo.png?h=30',
+      src: 'https://base.imgix.net/files/base/ebm/tdworld/image/static/tdworld-logo.png?h=30',
       srcset: [
-        'https://base.imgix.net/files/base/ebm/iw/static/iw_logo.png?h=60 2x',
+        'https://base.imgix.net/files/base/ebm/tdworld/image/static/tdworld-logo.png?h=60 2x',
       ],
     },
     footer: {
-      src: 'https://base.imgix.net/files/base/ebm/iw/static/iw_logo.png?h=45',
+      src: 'https://base.imgix.net/files/base/ebm/tdworld/image/static/tdworld-logo.png?h=45',
       srcset: [
-        'https://base.imgix.net/files/base/ebm/iw/static/iw_logo.png?h=90 2x',
+        'https://base.imgix.net/files/base/ebm/tdworld/image/static/tdworld-logo.png?h=90 2x',
       ],
     },
   },
   socialMediaLinks: [
-    { provider: 'facebook', href: 'https://www.facebook.com/pages/IndustryWeek/91310488418', target: '_blank' },
-    { provider: 'twitter', href: 'https://twitter.com/IndustryWeek', target: '_blank' },
-    { provider: 'linkedin', href: 'https://www.linkedin.com/company/industry-week', target: '_blank' },
-    { provider: 'youtube', href: 'https://www.youtube.com/user/industryweek', target: '_blank' },
+    { provider: 'facebook', href: 'https://www.facebook.com/tdworldmag/', target: '_blank' },
+    { provider: 'twitter', href: 'https://twitter.com/tdworldmag', target: '_blank' },
+    { provider: 'linkedin', href: 'https://www.linkedin.com/groups/4078692', target: '_blank' },
+    { provider: 'youtube', href: 'https://www.youtube.com/channel/UCD6JFCCOwOPu2v-KBcww_qw', target: '_blank' },
   ],
   gtm: {
-    containerId: 'GTM-MQVBJZ5',
+    containerId: 'GTM-KPZQF6G',
   },
   wufoo: {
     userName: 'cygnuscorporate',
-  },
-  magazines: {
-    description: '',
   },
 };
