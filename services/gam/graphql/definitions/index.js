750f38f7540f410f850c8a941001d5099920d104
Jacob Bare <jacob@limit0.io>
2019-11-19 14:46:31 -0600

    Add location, position, and pos target enums

--- services/gam/graphql/definitions/index.js
+++ services/gam/graphql/definitions/index.js
@@ -13,4 +13,98 @@ type Mutation {
   ping: String!
 }
 
+enum Locations {
+  article
+  author
+  commodities
+  forums_category
+  forums_landing
+  forums_topic
+  gallery
+  homepage
+  page
+  search
+  taxonomy
+  user
+  weather
+}
+
+enum Positions {
+  bottom_banner
+  cash_grain_bids
+  custom_content_channel_sponsored_header
+  custom_content_channel_sponsored_resources
+  hidden
+  inarticle1
+  inarticle2
+  inarticle3
+  inarticle4
+  inarticlevid
+  infinitescroll
+  interstitial
+  jumbotron
+  left_rail_rect
+  native_article_4
+  native_category_1
+  native_category_2
+  native_category_3
+  native_inline
+  native_leftrail_1
+  native_leftrail_2
+  native_related
+  native_top_stories
+  native_top_stories_1
+  native_umpu
+  reveal
+  right_col_jumbotron
+  right_rail_rect
+  sponsored_logo
+  sponsored_logo_commodities
+  sponsored_logo_weather
+  third_cat_jumbotron
+  top_banner
+}
+
+# NOTE: Enums cannot start with a number string.
+# As such, position targets starting with a number have been prefixed with
+# an underscore.
+enum PosTargets {
+  _300_1_lft
+  _300_1_rht
+  _300_2_lft
+  _300_2_rht
+  _300_3_rht
+  _300_4_rht
+  _728_10_a
+  _728_1_a
+  _728_2_a
+  adhesion
+  cashgrainwidgetlogo
+  commodwidgetlogo
+  customsponsoredlogo1
+  customsponsoredlogo2
+  footnote
+  footnote,floor
+  gallery_300_right
+  gallery_728_a
+  gallery_interstitial
+  inarticlevideo_1_a
+  nativekey_10
+  nativekey_12_1
+  nativekey_13_1
+  nativekey_1_lft
+  nativekey_2_lft
+  nativekey_3
+  nativekey_4
+  nativekey_5
+  nativekey_6
+  nativekey_7
+  nativekey_8
+  oop_a
+  reveal_1
+  sponsoredlogo
+  sponsoredlogo_weather
+  wrap
+}
+
 `;
