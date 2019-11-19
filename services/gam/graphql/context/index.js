43ae26d6f0635b64fca55b39d690df03978508c6
Jacob Bare <jacob@limit0.io>
2019-11-19 16:16:07 -0600

    Set mongo and base db clients to context

--- services/gam/graphql/context/index.js
+++ services/gam/graphql/context/index.js
@@ -1,10 +1,18 @@
+const { createBaseDB } = require('@base-cms/db');
+const mongodb = require('../../mongodb');
+
 module.exports = async ({ req }) => {
   const { query } = req;
   const tenantKey = req.get('x-tenant-key') || query['tenant-key'];
   const siteId = req.get('x-site-id') || query['site-id'];
   if (!tenantKey || !siteId) throw new Error('A tenant key and site ID are required.');
+
+  const basedb = createBaseDB({ tenant: tenantKey, client: mongodb });
+
   return {
     tenantKey,
     siteId,
+    mongodb,
+    basedb,
   };
 };
