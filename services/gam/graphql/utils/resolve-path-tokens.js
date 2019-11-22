1007f726ea0a273a58c4cb7176d9bbadb9de6600
Jacob Bare <jacob@limit0.io>
2019-11-22 09:12:07 -0600

    Create initial path token resolvers

---
+++ services/gam/graphql/utils/resolve-path-tokens.js
@@ -0,0 +1,53 @@
+const { UserInputError } = require('apollo-server-express');
+const loadSectionHierarchy = require('./load-section-hierarchy');
+const cleanPathValue = require('./clean-path-value');
+
+const contextError = type => new UserInputError(`A ${type} context is required to generate the ad unit path`);
+
+const resolvers = {
+  /**
+   * The DFP/GAM instance ID + site root path.
+   * This will be handled by the website and can removed.
+   */
+  '[dfp_tag:site_id]': () => '',
+
+  /**
+   * The Drupal vocab name. In our case, can be hardcoded to `categories`
+   * Used on `taxonomy` and `article` locations
+   */
+  '[term:vocabulary:machine-name]': () => 'categories',
+
+  /**
+   * @todo This field is not being saved in the legacy data. Must be added.
+   * For now, return nothing.
+   */
+  '[term:field_penton_native_advertising:/]': () => '',
+
+  /**
+    * The term (with its parents) merged with forward-slashes
+    * Similar to primary category parents join.
+    * Used on `taxonomy` and `article` locations
+    *
+    * @param {object} ctx
+    * @param {object} ctx.section The website section context.
+    * @param {object} ctx.loaders The dataloaders object.
+   */
+  '[term:name_lowercase_without_spaces_specialchars]': async ({ section, loaders }) => {
+    if (!section) throw contextError('section');
+    const loader = loaders('websiteSection');
+    const hierarchy = await loadSectionHierarchy(section, loader);
+    const names = hierarchy.map(s => cleanPathValue(s.name));
+    console.log(names);
+    return names.join('/');
+  },
+};
+
+module.exports = async (path, ctx) => {
+  const tokens = path.match(/\[.*?:.+?\]/g);
+  return Promise.all(tokens.map(async (token) => {
+    const resolver = resolvers[token];
+    if (!resolver) throw new Error(`No path token resolver was found for ${token}`);
+    const replacement = await resolver(ctx);
+    return { pattern: token, replacement };
+  }));
+};
