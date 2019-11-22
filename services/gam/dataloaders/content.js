49b7225d237c16de10278732663ce1d66d079793
Jacob Bare <jacob@limit0.io>
2019-11-22 13:20:51 -0600

    Add additional projection fields for new tokemns

--- services/gam/dataloaders/content.js
+++ services/gam/dataloaders/content.js
@@ -2,8 +2,12 @@ const DataLoader = require('dataloader');
 const createBatchFn = require('./utils/create-batch-fn');
 
 const projection = {
+  type: 1,
+  taxonomy: 1,
   'mutations.Website.primarySection': 1,
   'legacy.raw.field_penton_native_advertising.und': 1,
+  'legacy.id': 1,
+  'legacy.type': 1,
 };
 
 module.exports = ({ basedb }) => new DataLoader(keys => createBatchFn({ model: 'platform.Content', projection, basedb })(keys));
