0e5416253d094564c3fa4fddfb50b785ec0310df
Jacob Bare <jacob@limit0.io>
2019-11-25 12:48:58 -0600

    Include content name and authors

--- services/gam/dataloaders/content.js
+++ services/gam/dataloaders/content.js
@@ -2,8 +2,10 @@ const DataLoader = require('dataloader');
 const createBatchFn = require('./utils/create-batch-fn');
 
 const projection = {
+  name: 1,
   type: 1,
   taxonomy: 1,
+  authors: 1,
   'mutations.Website.primarySection': 1,
   'legacy.raw.field_penton_native_advertising.und': 1,
   'legacy.raw.field_penton_program.und': 1,
