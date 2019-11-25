824f69dba1005117dad5710d08a5c6bdc7a87e28
Jacob Bare <jacob@limit0.io>
2019-11-25 11:58:53 -0600

    Add support for different id field

--- services/gam/dataloaders/utils/create-batch-fn.js
+++ services/gam/dataloaders/utils/create-batch-fn.js
@@ -1,6 +1,11 @@
 const reduceAndMap = require('./reduce-and-map');
 
-module.exports = ({ model, projection, basedb }) => async (ids) => {
-  const docs = await basedb.find(model, { _id: { $in: ids } }, { projection });
-  return reduceAndMap({ ids, docs });
+module.exports = ({
+  model,
+  projection,
+  basedb,
+  field = '_id',
+}) => async (ids) => {
+  const docs = await basedb.find(model, { [field]: { $in: ids } }, { projection });
+  return reduceAndMap({ ids, docs, field });
 };
--- services/gam/dataloaders/utils/reduce-and-map.js
+++ services/gam/dataloaders/utils/reduce-and-map.js
@@ -1,6 +1,9 @@
-module.exports = ({ ids, docs }) => {
+const { get } = require('@base-cms/object-path');
+
+module.exports = ({ ids, docs, field = '_id' }) => {
   const mapped = docs.reduce((map, doc) => {
-    map.set(`${doc._id}`, doc);
+    const id = get(doc, field);
+    map.set(`${id}`, doc);
     return map;
   }, new Map());
   return ids.map(id => (mapped.get(`${id}`) || null));
