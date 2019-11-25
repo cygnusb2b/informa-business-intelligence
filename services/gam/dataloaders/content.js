4649ccd3ee310a57282d64ed50806ac5d83b7ffd
Jacob Bare <jacob@limit0.io>
2019-11-25 11:59:21 -0600

    Add additional raw fields

--- services/gam/dataloaders/content.js
+++ services/gam/dataloaders/content.js
@@ -6,8 +6,9 @@ const projection = {
   taxonomy: 1,
   'mutations.Website.primarySection': 1,
   'legacy.raw.field_penton_native_advertising.und': 1,
+  'legacy.raw.field_penton_program.und': 1,
+  'legacy.raw.field_penton_article_type.und': 1,
   'legacy.id': 1,
-  'legacy.type': 1,
 };
 
 module.exports = ({ basedb }) => new DataLoader(keys => createBatchFn({ model: 'platform.Content', projection, basedb })(keys));
