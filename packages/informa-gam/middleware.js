334f31782976c832412066d6b448910b3108e60a
Jacob Bare <jacob@limit0.io>
2019-11-26 08:03:50 -0600

    Cleanup adunit loading into util functions

--- packages/informa-gam/middleware.js
+++ packages/informa-gam/middleware.js
@@ -4,13 +4,29 @@ const gql = require('graphql-tag');
 const { ApolloClient } = require('apollo-client');
 const { InMemoryCache } = require('apollo-cache-inmemory');
 const { createHttpLink } = require('apollo-link-http');
-const { asObject } = require('@base-cms/utils');
+const { asObject, asArray } = require('@base-cms/utils');
 const { dasherize } = require('@base-cms/inflector');
+const { getAsArray } = require('@base-cms/object-path');
 const GAMConfiguration = require('@base-cms/marko-web-gam/config');
 
 const { GAM_SERVICE_URI, TENANT_KEY } = require('./env');
 
 const createCacheKey = (location, context) => `${location}|${JSON.stringify(context || {})}`;
+
+const mapSizes = size => asArray(size).map(({ width, height }) => [width, height]);
+
+const buildAdunit = ({ unit, config, globalTargeting }) => ({
+  ...unit,
+  path: config.createAdUnitPath(unit.path),
+  size: mapSizes(unit.size),
+  sizeMapping: getAsArray(unit, 'sizeMapping').map(({ viewport: vp, size }) => ({ viewport: [vp.width, vp.height], size: mapSizes(size) })),
+  targeting: { ...unit.targeting, ...globalTargeting },
+});
+
+const contextHeaders = context => Object.keys(asObject(context)).reduce((o, prop) => {
+  const header = `x-${dasherize(prop)}`;
+  return { ...o, [header]: context[prop] };
+}, {});
 
 const query = gql`
 
@@ -19,6 +35,7 @@ query LoadAdUnits($input: LocationAdunitsQueryInput!) {
     targeting
     adunits {
       id
+      key
       path
       position
       size {
@@ -57,20 +74,13 @@ module.exports = ({ accountId, basePath } = {}) => (req, res, next) => {
     const key = createCacheKey(location, context);
     if (!cache.has(key)) {
       const input = { location };
-      const headers = Object.keys(asObject(context)).reduce((o, prop) => {
-        const header = `x-${dasherize(prop)}`;
-        return { ...o, [header]: context[prop] };
-      }, {});
+      const headers = contextHeaders(context);
       const promise = apollo.query({ query, variables: { input }, context: { headers } })
         .then(({ data }) => data.locationAdunits)
-        .then(({ targeting, adunits }) => adunits.reduce((map, unit) => {
+        .then(({ targeting: globalTargeting, adunits }) => adunits.reduce((map, unit) => {
           const { position } = unit;
           if (!map.has(position)) map.set(position, []);
-          map.get(position).push({
-            ...unit,
-            path: config.createAdUnitPath(unit.path),
-            targeting: { ...unit.targeting, ...targeting },
-          });
+          map.get(position).push(buildAdunit({ unit, config, globalTargeting }));
           return map;
         }, new Map()));
       cache.set(key, promise);
