65a0edb92a4d2a928a344da8c3640f490a7a2185
Jacob Bare <jacob@limit0.io>
2019-11-25 20:52:47 -0600

    Add context support to informa ad unit comp

--- packages/informa-gam/middleware.js
+++ packages/informa-gam/middleware.js
@@ -4,6 +4,8 @@ const gql = require('graphql-tag');
 const { ApolloClient } = require('apollo-client');
 const { InMemoryCache } = require('apollo-cache-inmemory');
 const { createHttpLink } = require('apollo-link-http');
+const { asObject } = require('@base-cms/utils');
+const { dasherize } = require('@base-cms/inflector');
 const GAMConfiguration = require('@base-cms/marko-web-gam/config');
 
 const { GAM_SERVICE_URI, TENANT_KEY } = require('./env');
@@ -52,29 +54,27 @@ module.exports = ({ accountId, basePath } = {}) => (req, res, next) => {
     cache: new InMemoryCache(),
   });
   const adunit = (location, context) => {
-    console.log('loading ad unit for', location);
     const key = createCacheKey(location, context);
     if (!cache.has(key)) {
-      console.log('not in cache! setting for', location);
       const input = { location };
-      const promise = apollo.query({ query, variables: { input } })
+      const headers = Object.keys(asObject(context)).reduce((o, prop) => {
+        const header = `x-${dasherize(prop)}`;
+        return { ...o, [header]: context[prop] };
+      }, {});
+      const promise = apollo.query({ query, variables: { input }, context: { headers } })
         .then(({ data }) => data.locationAdunits)
-        .then(({ targeting, adunits }) => {
-          const adunitMap = adunits.reduce((map, unit) => {
-            const { position } = unit;
-            if (!map.has(position)) map.set(position, []);
-            map.get(position).push({
-              ...unit,
-              path: config.createAdUnitPath(unit.path),
-              targeting: { ...unit.targeting, ...targeting },
-            });
-            return map;
-          }, new Map());
-          return adunitMap;
-        });
+        .then(({ targeting, adunits }) => adunits.reduce((map, unit) => {
+          const { position } = unit;
+          if (!map.has(position)) map.set(position, []);
+          map.get(position).push({
+            ...unit,
+            path: config.createAdUnitPath(unit.path),
+            targeting: { ...unit.targeting, ...targeting },
+          });
+          return map;
+        }, new Map()));
       cache.set(key, promise);
     }
-    console.log('found in cache', location);
     return cache.get(key);
   };
 
