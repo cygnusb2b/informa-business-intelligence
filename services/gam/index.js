c4722beb9aef3fd1bf72ea488c03e7bfac02ab23
Jacob Bare <jacob@limit0.io>
2019-11-26 15:11:02 -0600

    Add newrelic to gam service

--- services/gam/index.js
+++ services/gam/index.js
@@ -4,12 +4,11 @@ const app = require('./app');
 const env = require('./env');
 const services = require('./services');
 const pkg = require('./package.json');
+const newrelic = require('./nr');
 
 const { log } = console;
 const { INTERNAL_PORT, EXTERNAL_PORT } = env;
 const server = http.createServer(app);
-
-process.on('unhandledRejection', (e) => { throw e; });
 
 app.get('/', (req, res) => {
   res.json({ ping: 'pong' });
@@ -32,5 +31,14 @@ const run = async () => {
   server.listen(INTERNAL_PORT, () => log(`> Ready on http://0.0.0.0:${EXTERNAL_PORT}`));
 };
 
+process.on('unhandledRejection', (e) => {
+  log('> Unhandled promise rejection. Throwing error...');
+  newrelic.noticeError(e);
+  throw e;
+});
+
 log(`> Booting ${pkg.name} v${pkg.version}...`);
-run().catch(e => setImmediate(() => { throw e; }));
+run().catch(e => setImmediate(() => {
+  newrelic.noticeError(e);
+  throw e;
+}));
