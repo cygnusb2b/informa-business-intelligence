4a9f11ceb177ba66e0bc673c1e7e5c01a59bb483
Jacob Bare <jacob@limit0.io>
2019-12-07 18:21:11 -0600

    Add element class name util and use

--- packages/lazarus-shared/browser/gallery-slideshow/slide.vue
+++ packages/lazarus-shared/browser/gallery-slideshow/slide.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="gallery-slideshow__slide">
-    <div class="gallery-slideshow__image">
-      <img :src="image.src" :alt="image.alt">
-      <div v-if="image.credit" class="gallery-slideshow__credit">
+  <div :class="element('slide')">
+    <div :class="element('image-wrapper')">
+      <img :src="image.src" :alt="image.alt" :class="element('image')">
+      <div v-if="image.credit" :class="element('credit')">
         {{ image.credit }}
       </div>
     </div>
@@ -12,14 +12,14 @@
     <button v-if="hasNext" @click="$emit('next')">
       Next &gt;
     </button>
-    <div class="gallery-slideshow__slide-position">
+    <div :class="element('slide-count')">
       Slide {{ slideNumber }} of {{ slideCount }}
     </div>
-    <h3 class="gallery-slideshow__title">
+    <h3 :class="element('title')">
       {{ image.displayName }}
     </h3>
     <!-- eslint-disable-next-line vue/no-v-html -->
-    <div class="gallery-slideshow__body" v-html="image.body" />
+    <div :class="element('body')" v-html="image.body" />
   </div>
 </template>
 
@@ -46,6 +46,12 @@ export default {
     },
     hasPrevious() {
       return this.slideNumber > 1;
+    },
+  },
+
+  methods: {
+    element(name) {
+      return `gallery-slideshow__${name}`;
     },
   },
 };
