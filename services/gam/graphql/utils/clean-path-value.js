e77976ea4011446c0c7eec914ce85c05a303d1b7
Jacob Bare <jacob@limit0.io>
2019-12-10 11:08:04 -0600

    Replace multiple underscores last
    Ensures that multiple underscores are properly removed.

--- services/gam/graphql/utils/clean-path-value.js
+++ services/gam/graphql/utils/clean-path-value.js
@@ -2,11 +2,11 @@
 module.exports = (value) => {
   // Replace forward-slashes, spaces, commas, and dashes with underscores
   let v = value.replace(/[\s,-/]/g, '_');
-  // Replace multiple underscores with one.
-  v = v.replace(/_{2,}/g, '_');
   // Replace special characters (as defined by GAM).
   v = v.replace(/["'=!+#*~;^()[\]<>,.&]/g, '');
   // Replace additional special chars (as defined by Penton PHP code).
   v = v.replace(/|\?“‘’”…{}/g, '');
+  // Replace multiple underscores with one.
+  v = v.replace(/_{2,}/g, '_');
   return v.toLowerCase();
 };
