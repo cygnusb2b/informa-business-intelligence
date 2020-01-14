9e2162ce54bbde0262990c4788a00eb10c561e05
Jacob Bare <jacob@limit0.io>
2020-01-14 10:39:20 -0600

    Create initial directory page template + routes

--- packages/lazarus-shared/routes/website-section.js
+++ packages/lazarus-shared/routes/website-section.js
@@ -1,12 +1,24 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
 const contactUs = require('../templates/website-section/contact-us');
+const directory = require('../templates/website-section/directory');
 const queryFragment = require('../graphql/fragments/website-section-page');
+const directoryFragment = require('../graphql/fragments/directory-page');
 
 module.exports = (app) => {
   app.get('/:alias(contact-us)', withWebsiteSection({
     template: contactUs,
     queryFragment,
+  }));
+
+  app.get('/:alias(directory)', withWebsiteSection({
+    template: directory,
+    queryFragment: directoryFragment,
+  }));
+
+  app.get('/:alias(directory/[a-z0-9-/]+)', withWebsiteSection({
+    template: directory,
+    queryFragment: directoryFragment,
   }));
 
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
