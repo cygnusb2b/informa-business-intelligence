11dfe64125851aa12a5fa339c747f4d8d262e19c
Jacob Bare <jacob@limit0.io>
2020-01-14 12:30:13 -0600

    Set initial facet props

--- packages/lazarus-shared/browser/directory-facets/index.vue
+++ packages/lazarus-shared/browser/directory-facets/index.vue
@@ -1,11 +1,28 @@
 <template>
-  <div />
+  <div
+    class="directory-facets"
+    :data-root-alias="rootAlias"
+    :data-active-alias="activeAlias"
+  />
 </template>
 
 <script>
 export default {
+  /**
+   *
+   */
+  props: {
+    rootAlias: {
+      type: String,
+      required: true,
+    },
+    activeAlias: {
+      type: String,
+      default: null,
+    },
+  },
   created() {
-    console.log('directory facets created!');
+    console.log({ rootAlias: this.rootAlias, activeAlias: this.activeAlias });
   },
 };
 </script>
