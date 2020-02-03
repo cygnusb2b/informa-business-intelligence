f5935e76507198eabb0a60bbe8225fbb9a908948
Brian Miller <bmiller@southcomm.com>
2020-02-03 11:17:38 -0600

    remove company and contact load more components

--- packages/lazarus-shared/components.js
+++ packages/lazarus-shared/components.js
@@ -1,13 +1,9 @@
 const LazarusSharedContentLoadMoreFlow = require('./components/flows/content-load-more');
-const LazarusSharedCompanyPageLoadMoreFlow = require('./components/flows/company-page-load-more');
-const LazarusSharedContactPageLoadMoreFlow = require('./components/flows/contact-page-load-more');
 const LazarusSharedContentPageLoadMoreFlow = require('./components/flows/content-page-load-more');
 const LazarusSharedContentListFlow = require('./components/flows/content-list');
 
 module.exports = {
   LazarusSharedContentLoadMoreFlow,
-  LazarusSharedCompanyPageLoadMoreFlow,
-  LazarusSharedContactPageLoadMoreFlow,
   LazarusSharedContentPageLoadMoreFlow,
   LazarusSharedContentListFlow,
 };
