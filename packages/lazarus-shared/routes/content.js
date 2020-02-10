8d460cb1ba84314292fd5665d74a3ad262e7143a
Jacob Bare <jacob@limit0.io>
2020-02-10 12:21:36 -0600

    Create initial print template

--- packages/lazarus-shared/routes/content.js
+++ packages/lazarus-shared/routes/content.js
@@ -1,11 +1,17 @@
 const { withContent } = require('@base-cms/marko-web/middleware');
 const content = require('../templates/content');
+const print = require('../templates/content/print');
 const slideshow = require('../templates/content/slideshow');
 const fullWidth = require('../templates/content/full-width');
 const queryFragment = require('../graphql/fragments/content-page');
 const slideshowFragment = require('../graphql/fragments/content-slideshow-page');
 
 module.exports = (app) => {
+  app.get('/print/content/:id(\\d{8})', withContent({
+    template: print,
+    queryFragment,
+    redirectOnPathMismatch: false,
+  }));
   app.get('/*?(whitepaper|webinar)/:id(\\d{8})*', withContent({
     template: fullWidth,
     queryFragment,
