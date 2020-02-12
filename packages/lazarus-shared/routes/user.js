2ca1e925681c3d27a918f8bc60819e9824ad4839
Jacob Bare <jacob@limit0.io>
2020-02-12 13:17:10 -0600

    Add user profile template and route

--- packages/lazarus-shared/routes/user.js
+++ packages/lazarus-shared/routes/user.js
@@ -3,6 +3,7 @@ const IdentityXConfig = require('@base-cms/marko-web-identity-x/config');
 const authenticate = require('../templates/user/authenticate');
 const login = require('../templates/user/login');
 const logout = require('../templates/user/logout');
+const profile = require('../templates/user/profile');
 const register = require('../templates/user/register');
 
 const { isArray } = Array;
@@ -16,6 +17,7 @@ module.exports = (app) => {
 
     const navConfig = [
       { href: config.getEndpointFor('login'), label: 'Log In', when: 'logged-out' },
+      { href: config.getEndpointFor('profile'), label: 'My Profile', when: 'logged-in' },
       { href: config.getEndpointFor('logout'), label: 'Log Out', when: 'logged-in' },
       { href: config.getEndpointFor('register'), label: 'Register', when: 'logged-out' },
     ];
@@ -27,5 +29,6 @@ module.exports = (app) => {
     app.get(config.getEndpointFor('login'), (_, res) => { res.marko(login); });
     app.get(config.getEndpointFor('logout'), (_, res) => { res.marko(logout); });
     app.get(config.getEndpointFor('register'), (_, res) => { res.marko(register); });
+    app.get(config.getEndpointFor('profile'), (_, res) => { res.marko(profile); });
   }
 };
