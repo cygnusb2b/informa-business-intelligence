97684d8bf7bde372cc4a2808e21e661dae63b980
Brandon Krigbaum <brandonbk@gmail.com>
2020-03-06 08:11:58 -0600

    Add digital edition route

--- packages/lazarus-shared/routes/digital-edition.js
+++ packages/lazarus-shared/routes/digital-edition.js
@@ -1,7 +1,7 @@
 const digitalEdition = require('../templates/digital-edition');
 
 module.exports = (app) => {
-  app.get('/digital-edition', (_, res) => {
-    res.marko(digitalEdition);
+  app.get('/digital-edition/zmags/:id([a-fA-F0-9]{8})', (req, res) => {
+    res.marko(digitalEdition, { id: req.params.id });
   });
 };
