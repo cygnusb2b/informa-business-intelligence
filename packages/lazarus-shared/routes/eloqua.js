828eee5d8292155c0bdfff96efa8f04d74b80f02
jlaird <Jeremy.Laird@cygnus.com>
2020-03-18 10:58:30 -0500

    Cleaned syntax

--- packages/lazarus-shared/routes/eloqua.js
+++ packages/lazarus-shared/routes/eloqua.js
@@ -1,18 +1,19 @@
 const fetch = require('node-fetch');
-const asyncRoute = require('@base-cms/utils/src/async-route.js');
+const asyncRoute = require('@base-cms/utils/src/async-route');
 
 module.exports = (app) => {
   app.get('/__eloqua/form/:formId', asyncRoute(async (req, res) => {
     const auth = Buffer.from(`${process.env.ELOQUA_SITE_NAME}\\${process.env.ELOQUA_USER}:${process.env.ELOQUA_PASSWORD}`).toString('base64');
-    const data = await fetch(
+    const response = await fetch(
       `https://secure.p01.eloqua.com/api/REST/2.0/assets/form/${req.params.formId}`,
       {
         headers: {
-          'Content-Type': 'application/json',
+          'content-type': 'application/json',
           Authorization: `Basic ${auth}`,
         },
       },
-    ).then(r => r.json());
-    res.json({ html: data.html });
+    );
+    const json = await response.json();
+    res.json({ html: json.html });
   }));
 };
