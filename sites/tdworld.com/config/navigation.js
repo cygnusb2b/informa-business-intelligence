c90fcfb168de5aa8d30286ce310522ff7175f4b6
Alysha Buselt <>
2020-04-06 22:16:22 -0400

    Updated advertise links in legacy informa footers
    https://www.pivotaltracker.com/story/show/172091396

--- sites/tdworld.com/config/navigation.js
+++ sites/tdworld.com/config/navigation.js
@@ -10,7 +10,7 @@ module.exports = {
     items: [
       { href: '/page/about-us', label: 'About Us' },
       { href: '/page/contact-us', label: 'Contact Us' },
-      { href: 'https://power.informa.com/', label: 'Advertise', target: '_blank' },
+      { href: 'https://energy.endeavorb2b.com/tdworld', label: 'Advertise', target: '_blank' },
       { href: 'https://endeavor.dragonforms.com/loading.do?omedasite=EBM_DoNotSell', label: 'California Do Not Sell', target: '_blank' },
       { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy & Cookie Policy', target: '_blank' },
       { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms of Service', target: '_blank' },
