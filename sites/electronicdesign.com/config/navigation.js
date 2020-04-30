182ffb85cdd3c45d4ada7e2f5da4d3a96274295f
Alysha Buselt <>
2020-04-30 15:15:34 -0400

    Update ED navigation
    https://www.pivotaltracker.com/story/show/172567943
    
    - move ‘Resources’ to the top of list
    - update ‘Advertise’ url

--- sites/electronicdesign.com/config/navigation.js
+++ sites/electronicdesign.com/config/navigation.js
@@ -19,13 +19,13 @@ module.exports = {
   menu: [
     {
       items: [
+        { href: '/page/resources', label: 'Resources' },
         { href: '/markets/automotive', label: 'Automotive' },
         { href: '/industrial-automation', label: 'Industrial Automation' },
         { href: '/technologies/analog', label: 'Analog' },
         { href: '/power-management', label: 'Power Management' },
         { href: '/technologies/embedded-revolution', label: 'Embedded Revolution' },
         { href: '/technologies/test-measurement', label: 'Test & Measurement' },
-        { href: '/page/resources', label: 'Resources' },
         { href: '/blogs', label: 'Experts' },
         { href: '/covid-19', label: 'COVID-19' },
       ],
@@ -43,7 +43,7 @@ module.exports = {
         { href: dragonForms.getFormUrl('newsletterSignup'), label: 'eNewsletter Subscription', target: '_blank' },
         { href: '/page/about-us', label: 'About Us' },
         { href: '/page/contact-us', label: 'Contact Us' },
-        { href: 'https://designengineering.endeavorb2b.com/electronic-design', label: 'Advertise', target: '_blank' },
+        { href: 'https://designengineering.endeavorb2b.com/electronic-design-2/', label: 'Advertise', target: '_blank' },
         { href: '/page/contribute', label: 'Contribute' },
         { href: 'https://electronics.informa.com/contentstream/', label: 'Content Licensing', target: '_blank' },
         { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy & Cookie Policy', target: '_blank' },
