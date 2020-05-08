27ef54181787297fde6b99b0bc56e0fccf842a80
Alysha Buselt <>
2020-05-08 10:17:24 -0400

    Update advertise links
    https://www.pivotaltracker.com/story/show/172682508

--- sites/fleetowner.com/config/navigation.js
+++ sites/fleetowner.com/config/navigation.js
@@ -10,7 +10,7 @@ module.exports = {
     items: [
       { href: '/page/about-us', label: 'About Us' },
       { href: '/page/about-us', label: 'Contact Us' },
-      { href: 'https://cv.endeavorb2b.com/fleet-owner/', label: 'Advertise', target: '_blank' },
+      { href: 'http://link.argifocus.com/0DI-24', label: 'Advertise', target: '_blank' },
       { href: 'https://endeavor.dragonforms.com/loading.do?omedasite=EBM_DoNotSell', label: 'California Do Not Sell', target: '_blank' },
       { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy & Cookie Policy', target: '_blank' },
       { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms of Service', target: '_blank' },
@@ -47,7 +47,7 @@ module.exports = {
         { href: '/resource-center/powertrain', label: 'Powertrain' },
         { href: '/news/regulations', label: 'Regulation' },
         { href: '/page/about-us', label: 'About Us / Contact Us' },
-        { href: 'https://cv.endeavorb2b.com/fleet-owner/', label: 'Advertiser Info', target: '_blank' },
+        { href: 'http://link.argifocus.com/0DI-24', label: 'Advertiser Info', target: '_blank' },
         { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy & Cookie Policy', target: '_blank' },
         { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms of Service', target: '_blank' },
       ],
