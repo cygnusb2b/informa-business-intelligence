307686a57b58f0052bdf6adfe58b9de9938eba8d
Alysha Buselt <>
2020-05-08 10:35:39 -0400

    Add magazine subscribe url
    https://www.pivotaltracker.com/story/show/172682576

--- sites/refrigeratedtransporter.com/config/dragon-forms.js
+++ sites/refrigeratedtransporter.com/config/dragon-forms.js
@@ -4,7 +4,7 @@ const config = new DragonFormsConfig({ url: 'https://endeavor.dragonforms.com' }
 
 config
   .addForm('newsletterSubscribe', { omedasite: 'RFTnewpref' })
-  .addForm('newsletterSignup', { omedasite: 'RFTnewpref', query: { pk: 'ARTWEB' } });
-//   .addForm('magazineSignup', { omedasite: 'PEN6133_ASland', query: { pk: 'NNHWEB' } });
+  .addForm('newsletterSignup', { omedasite: 'RFTnewpref', query: { pk: 'ARTWEB' } })
+  .addForm('magazineSignup', { omedasite: 'RT_renew' });
 
 module.exports = config;
