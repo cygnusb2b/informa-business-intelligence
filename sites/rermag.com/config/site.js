fd833fd78468524d2999013c401b3e02774740d3
Jacob Bare <jacob@limit0.io>
2020-06-25 16:17:58 -0500

    Add inquiry to company pages; enable on RER

--- sites/rermag.com/config/site.js
+++ sites/rermag.com/config/site.js
@@ -46,4 +46,17 @@ module.exports = {
     userName: 'cygnuscorporate',
   },
   newsletterSubscribeLink: dragonForms.getFormUrl('newsletterSubscribe'),
+  inquiry: {
+    // the subject line of the email sent to the brand and/or the company/advertiser
+    notificationSubject: 'A new inquiry submission was received.',
+    // the subject line of the email sent to the user who submitted the form
+    confirmationSubject: 'Your inquiry was received.',
+    enabled: true,
+    directSend: false, // enable this to directly send to the company
+    sendTo: 'notifications@rermag.com',
+    sendFrom: 'RERMag.com <noreply@baseplatform.io>',
+    sendBcc: 'emailactivity@cygnus.com',
+    logo: 'https://img.rermag.com/files/base/ebm/rermag/image/static/rermag-logo.png?h=60',
+    bgColor: '#ffffff',
+  },
 };
