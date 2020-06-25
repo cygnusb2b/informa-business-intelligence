b6f8a94edef5f9c3317f9fa12ec8032e87297555
Jacob Bare <jacob@limit0.io>
2020-06-25 16:18:54 -0500

    Create company load more

--- packages/lazarus-shared/components.js
+++ packages/lazarus-shared/components.js
@@ -1,10 +1,13 @@
 const LazarusSharedContentLoadMoreFlow = require('./components/flows/content-load-more');
+const LazarusSharedContentCardDeckFlow = require('./components/flows/content-card-deck');
+
 const LazarusSharedContentPageLoadMoreFlow = require('./components/flows/content-page-load-more');
 const LazarusSharedContentListFlow = require('./components/flows/content-list');
 const LazarusSharedMagazineIssueArchiveFlow = require('./components/flows/magazine-issue-archive');
 
 module.exports = {
   LazarusSharedContentLoadMoreFlow,
+  LazarusSharedContentCardDeckFlow,
   LazarusSharedContentPageLoadMoreFlow,
   LazarusSharedContentListFlow,
   LazarusSharedMagazineIssueArchiveFlow,
