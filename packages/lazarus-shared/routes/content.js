a6cd51782aa46d4f458af916f9f989b73e4c9c05
Jacob Bare <jacob@limit0.io>
2020-06-25 11:16:25 -0500

    Create initial company page layout

--- packages/lazarus-shared/routes/content.js
+++ packages/lazarus-shared/routes/content.js
@@ -1,10 +1,12 @@
 const { withContent } = require('@base-cms/marko-web/middleware');
 const content = require('../templates/content');
+const company = require('../templates/content/company');
 const print = require('../templates/content/print');
 const slideshow = require('../templates/content/slideshow');
 const fullWidth = require('../templates/content/full-width');
 const queryFragment = require('../graphql/fragments/content-page');
 const slideshowFragment = require('../graphql/fragments/content-slideshow-page');
+const companyFragment = require('../graphql/fragments/content-company-page');
 
 module.exports = (app) => {
   app.get('/print/content/:id(\\d{8})', withContent({
@@ -12,6 +14,12 @@ module.exports = (app) => {
     queryFragment,
     redirectOnPathMismatch: false,
   }));
+
+  app.get('/*?company/:id(\\d{8})*', withContent({
+    template: company,
+    queryFragment: companyFragment,
+  }));
+
   app.get('/*?(whitepaper|webinar)/:id(\\d{8})*', withContent({
     template: fullWidth,
     queryFragment,
