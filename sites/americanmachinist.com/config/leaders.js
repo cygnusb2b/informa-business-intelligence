790f499fa9c10a092e8c062af0843acc33c534dd
Alysha Buselt <>
2020-07-27 14:12:43 -0400

    Add leaders page to AM
    https://www.pivotaltracker.com/story/show/173992767

---
+++ sites/americanmachinist.com/config/leaders.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Leaders in Machining',
+  alias: 'leaders',
+  calloutValue: 'Leading Companies',
+  header: {
+    imgSrc: 'https://img.americanmachinist.com/files/base/ebm/americanmachinist/image/static/am_leaders.png?h=90',
+  },
+  // Remove this (or set to true) to enable integrated components on the home page and content page
+  // This has no effect on the leaders landing page
+  componentsEnabled: false,
+};
--- sites/americanmachinist.com/config/site.js
+++ sites/americanmachinist.com/config/site.js
@@ -1,10 +1,12 @@
 const navigation = require('./navigation');
 const dragonForms = require('./dragon-forms');
+const leaders = require('./leaders');
 
 module.exports = {
   company: 'Endeavor Business Media, LLC',
   navigation,
   dragonForms,
+  leaders,
   identityX: {
     enabled: true,
     appId: '5df0e82405aa56699b43fc2f',
