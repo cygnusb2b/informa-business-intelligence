178b09cd5e0a4a8b5eaf12719f9a79db56cbb908
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-21 10:31:45 -0500

    Disable lazy load on NED

--- sites/newequipment.com/config/site.js
+++ sites/newequipment.com/config/site.js
@@ -37,7 +37,7 @@ module.exports = {
     accountId: process.env.GAM_ACCCOUNT_ID || '3834',
     basePath: 'newequipment.home',
     lazyLoad: {
-      enabled: true, // set to true to enable lazy loading
+      enabled: false, // set to true to enable lazy loading
       fetchMarginPercent: 100, // fetch ad when one viewport away
       renderMarginPercent: 50, // render ad when half viewport away
       mobileScaling: 2, // double these on mobile
