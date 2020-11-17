4135c6034f1f4773bad7dd617ab170280218da8d
Brandon Krigbaum <brandonbk@gmail.com>
2020-11-17 13:35:23 -0600

    Rename “Contact Us” to “About Us”

--- sites/fleetmaintenance.com/config/navigation.js
+++ sites/fleetmaintenance.com/config/navigation.js
@@ -36,7 +36,7 @@ module.exports = {
         { href: dragonForms.getFormUrl('magazineSignup'), label: 'Magazine Subscription', target: '_blank' },
         { href: dragonForms.getFormUrl('newsletterSubscribe'), label: 'eNewsletter Subscription', target: '_blank' },
         { href: 'https://cv.endeavorb2b.com/fleet-maintenance/', label: 'Advertising Info', target: '_blank' },
-        { href: '/page/about-us', label: 'Contact Us' },
+        { href: '/page/about-us', label: 'About Us' },
         { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy & Cookie Policy', target: '_blank' },
         { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms of Service', target: '_blank' },
       ],
