86db2506e5614958a50fe34e97b1dfdb6e8c818d
Alysha Buselt <Alixers505@users.noreply.github.com>
2021-03-09 15:38:23 -0500

    Updated details to match machine design

--- sites/machinedesign.com/config/leaders.js
+++ sites/machinedesign.com/config/leaders.js
@@ -1,10 +1,10 @@
 module.exports = {
-  title: 'Connect with Leaders in Hydraulics & Pneumatics',
+  title: 'Connect with Leaders in Machine Design',
   description: 'Finding the right parts and suppliers has never been easier.',
-  alias: 'hpconnect',
-  calloutValue: 'Welcome to H&P Connect',
+  alias: 'leaders',
+  calloutValue: 'Welcome to Leaders in Machine Design',
   header: {
-    imgSrc: 'https://img.hydraulicspneumatics.com/files/base/ebm/hydraulicspneumatics/image/static/hp_leaders.png?h=90',
+    imgSrc: 'https://img.hydraulicspneumatics.com/files/base/ebm/machinedesign/image/static/Leaders-in-Distribution-MD.jpg?h=90',
   },
   // Remove this (or set to true) to enable integrated components on the home page and content page
   // This has no effect on the leaders landing page
