5371227f586653416c63727958b5159ba3f3895c
Alysha Buselt <Alixers505@users.noreply.github.com>
2021-03-09 15:07:59 -0500

    Added leaders file, referenced from HP

---
+++ sites/machinedesign.com/config/leaders.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Connect with Leaders in Hydraulics & Pneumatics',
+  description: 'Finding the right parts and suppliers has never been easier.',
+  alias: 'hpconnect',
+  calloutValue: 'Welcome to H&P Connect',
+  header: {
+    imgSrc: 'https://img.hydraulicspneumatics.com/files/base/ebm/hydraulicspneumatics/image/static/hp_leaders.png?h=90',
+  },
+  // Remove this (or set to true) to enable integrated components on the home page and content page
+  // This has no effect on the leaders landing page
+  componentsEnabled: false,
+};
