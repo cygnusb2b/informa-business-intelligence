f041e1e5737c3ca91af07b78b2b2ffb141b6ad59
Alysha Buselt <Alixers505@users.noreply.github.com>
2021-03-10 14:10:51 -0500

    Add leaders config to site.js

--- sites/machinedesign.com/config/site.js
+++ sites/machinedesign.com/config/site.js
@@ -1,10 +1,12 @@
 const navigation = require('./navigation');
 const dragonForms = require('./dragon-forms');
+const leaders = require('./leaders');
 
 module.exports = {
   company: 'Endeavor Business Media, LLC',
   navigation,
   dragonForms,
+  leaders,
   identityX: {
     enabled: true,
     appId: '5df0e90605aa5648c343fc38',
